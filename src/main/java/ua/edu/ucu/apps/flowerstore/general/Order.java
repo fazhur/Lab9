7140773db1e2c70b0111c7d58ff74146f02b828c
fazhur <fedir.zhurba@ucu.edu.ua>
2022-12-12 01:48:30 +0200

    Observer in added

--- src/main/java/ua/edu/ucu/apps/flowerstore/general/Order.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/general/Order.java
@@ -1,11 +1,24 @@
 package ua.edu.ucu.apps.flowerstore.general;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ua.edu.ucu.apps.flowerstore.user.MyUser;
 
 import java.util.List;
 
+@Getter @Setter @NoArgsConstructor
 public class Order {
-    @Getter
+    List<MyUser> users;
+    public void addUser(MyUser user) {
+        users.add(user);
+    }
+    public void removeUser(MyUser user) {
+        users.remove(user);
+    }
+    public void notifyUser(MyUser user) {
+//        send message to user
+    }
     private List<Item> order;
     public void addItem(Item item) {
         order.add(item);
